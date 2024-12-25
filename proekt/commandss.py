87da4155f828fa99054531bc6928e32608dd91f0
Bogdan-spit <bogd4n.grom@yandex.ru>
2024-12-25 22:38:27 +0300

    Update commandss.py

--- proekt/commandss.py
+++ proekt/commandss.py
@@ -3,7 +3,6 @@ import logging
 from aiogram import Bot, Dispatcher, types  
 from aiogram.filters.command import Command  
 from configs import Config 
-from random import randint 
 from aiogram.utils.keyboard import ReplyKeyboardBuilder 
 from aiogram.utils.keyboard import InlineKeyboardBuilder 
 import sqlite3
@@ -20,8 +19,7 @@ async def cmd_start(message: types.Message):
         [ 
             
             types.KeyboardButton(text="Первая помощь"),
-            types.KeyboardButton(text="test229"),
-            types.KeyboardButton(text="test230"),
+            types.KeyboardButton(text="Основы ЗОЖ"),
             types.KeyboardButton(text="test"),
             
             
@@ -36,31 +34,4 @@ async def cmd_start(message: types.Message):
 
 
 
-@router.message(Command("help")) 
-async def cmd_start(message: types.Message): 
-    kb = [ 
-        [ 
-            
-            types.KeyboardButton(text="Укусы"),
-            types.KeyboardButton(text="Не укусы"),
-            types.KeyboardButton(text="хз"),
-            # types.KeyboardButton(text="start"),
-            types.KeyboardButton(text="спс"),
-            
-            
-        ], 
-    ] 
-    keyboard = types.ReplyKeyboardMarkup( 
-        keyboard=kb, 
-        resize_keyboard=True, 
-        input_field_placeholder="Текст вместо 'Massage'" 
-    ) 
-    await message.answer("умный тест хз", reply_markup=keyboard) 
-
-def payment_keyboard():
-    types.KeyboardButton(text="Советы"),
-    types.KeyboardButton(text="test229"),
-    types.KeyboardButton(text="test230"),
-    # types.KeyboardButton(text="start"),
-    types.KeyboardButton(text="test"),
 
